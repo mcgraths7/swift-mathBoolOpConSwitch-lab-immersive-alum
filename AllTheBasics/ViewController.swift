c09755c6bb046ba2357e24fdb245c2b5e28ea4d3
mcgraths7 <mcgrath.steven7@gmail.com>
2018-08-08 18:41:31 -0400

    All tests passing

--- AllTheBasics/ViewController.swift
+++ AllTheBasics/ViewController.swift
@@ -10,7 +10,57 @@ import UIKit
 
 class ViewController: UIViewController {
     
+    func averageIsAbove75(a: Double, b: Double, c: Double) -> Bool {
+        if (a + b + c) / 3 > 75 {
+            return true
+        } else {
+            return false
+        }
+    }
     
+    func passwordCombo(username: String, password: Int) -> String {
+        var usernameIsValid = false
+        var passwordIsValid = false
+        
+        switch username {
+        case "Jerry":
+            usernameIsValid = true
+        case "Elaine":
+            usernameIsValid = true
+        case "Michael":
+            usernameIsValid = true
+        default:
+            break
+        }
+        
+        if password % 3 == 0 {
+            passwordIsValid = true
+        }
+        
+        if usernameIsValid && passwordIsValid {
+            return "Welcome!"
+        } else {
+            return "Access Denied"
+        }
+        
+    }
+    
+    func describe(emoji: String) -> String {
+        switch emoji {
+        case "💋":
+            return "Kiss"
+        case "🐈":
+            return "Cat"
+        case "🐢":
+            return "Turtle"
+        case "🍕":
+            return "Pizza"
+        case "👻":
+            return "Ghost"
+        default:
+            return "Unknown"
+        }
+    }
     
     
     
